dd614726e471cbac99e6050b4bd1fab00c926e72
smnjameson <smnjameson@googlemail.com>
2021-04-04 20:04:33 +0100

    Add files via upload

--- main.s
+++ main.s
@@ -2,113 +2,182 @@
 #import "../_include/m65macros.s"
 
 .label SCREEN_RAM = $10000
+// .label COLOR_RAM = $1f800
 .label COLOR_RAM = $ff80000
-
+.label NUMBER_OF_ROWS = $30
 
 * = $02 "Zeropage" virtual
 ZP: {
-	Screen:	
-		.dword $00000000
-	Color:
-		.dword $0ff80000
+	SCREEN: .dword $00000000
 }
 
 
 BasicUpstart65(Entry)
 * = $2016
-Entry: {
-		jsr SetupSystem
-        
-		jsr ScreenClear80
 
-		jsr SetupScreenVector
-		jsr SetupColorVector
-		//Move default screen location
-		//$d060-$d063
+Entry: {
+
+		jsr SetupM65System
+		jsr SetupVIC
+
+
+		jsr ImportPalette
+
+		lda #$00	//Char index LSB
+		ldx #$02	//Char index MSB
+		jsr ClearScreen16bit
+
+		lda #$00
+		jsr ClearColorRam
+
+		lda #$10
+		sta $d016
+
+		//D054 = Full color char modes
+		//bit 0 = 16bit char indices
+		//bit 1 = FCM for chars <=$ff
+		//bit 2 = FCM for chars > $ff
+		lda #%00000111
+		tsb $d054
+	!:	
+		jmp !-
+}
+
+
+ClearScreen16bit: {
+		sta LSB
+		stx MSB
+
 		lda #<SCREEN_RAM
+		sta ZP.SCREEN + 0
+		lda #>SCREEN_RAM
+		sta ZP.SCREEN + 1
+		lda #[[SCREEN_RAM >> 16] & $ff]
+		sta ZP.SCREEN + 2
+		lda #$00
+		sta ZP.SCREEN + 3
+
+		ldy #$19
+	!Loop:
+		ldz #$00
+	!:
+		lda LSB:#$00
+		sta ((ZP.SCREEN)), z
+		inz
+		lda MSB:#$00
+		sta ((ZP.SCREEN)), z
+		inz 
+		cpz #$50	//Size of a row in bytes
+		bne !-
+
+		clc 
+		lda ZP.SCREEN + 0 
+		adc #$50   //Size of a row in bytes
+		sta ZP.SCREEN + 0 
+		bcc !+
+		inw ZP.SCREEN + 1
+	!:
+
+
+		dey 
+		bne !Loop-
+		rts
+}
+
+
+/*
+	//increment a 32 bit value by 1 (VALUE + 0-3)
+
+		inq VALUE + 0
+		bne !+
+		inw VALUE + 2
+	!:
+*/
+
+ClearScreen: {
+		sta ClearScreenJob + 9 //This is the fill byte
+		RunDMAJob(ClearScreenJob)
+		rts
+
+	ClearScreenJob:
+		DMAHeader(0, SCREEN_RAM>>20) 
+		DMAFillJob($01, SCREEN_RAM, 40 * NUMBER_OF_ROWS * 2, false)
+}
+
+
+ClearColorRam: {
+		sta ClearColorRamJob + 9	
+		RunDMAJob(ClearColorRamJob)
+		rts
+
+	ClearColorRamJob:
+		DMAHeader($00, COLOR_RAM>>20 )
+		DMAFillJob($01, COLOR_RAM, 40 * NUMBER_OF_ROWS * 2, false)
+}
+
+
+
+
+SetupVIC: {
+		//relocate screen
+		lda #<SCREEN_RAM  //LSB #<
 		sta $d060
-		lda #>SCREEN_RAM 
+		lda #>SCREEN_RAM  //MSB #>
 		sta $d061
-		lda #[SCREEN_RAM >> 16]
+		lda #[[SCREEN_RAM >> 16] & $ff] //24bit MSB
 		sta $d062
 
-		//Relocate charset (Only for hires and MCM)
-		//$d068-$d06a 
-		// lda #<CHARROM
-		// sta $d068 
-		// lda #>CHARROM
-		// sta $d069
-		// lda #[CHARROM >> 16]
-		// sta $d06a
+		//40 column mode (clear bit 7 of D031)
+		lda #$80
+		trb $d031 
 
+		//Reposition start of top border
+		// lda #$00
+		// sta $d048
+		// //Reposition start of bottom border
+		// lda #$20
+		// sta $d04a
+		// lda #$02
+		// sta $d04b
+
+		// //Reposition start of screen at top
+		// lda #$10
+		// sta $d04e
+
+
+
+		rts
+}
+
+
+SetupM65System: {
+		sei 
 		
+		// Set memory layout (c64??)
+		lda #$35
+		sta $01
+		
+		enable40Mhz() 
+		enableVIC4Registers()
 
 		//Disable hot register so VIC2 registers 
 		//turn off bit 7 
 		lda #$80		
 		trb $d05d		//wont destroy VIC4 values (bit 7)
 
-		// Set VIC to use 40 column mode display
-		//turn off bit 7 
-		lda #$80		
-		trb $d031 
-
-		//Turn on MCM (same as C64)
-		// lda #$10
-		// tsb $d016
-
-		//Turn on FCM mode and
-		//16bit per char number
-		//bit 0 = Enable 16 bit char numbers
-		//bit 1 = Enable Fullcolor for chars <=$ff
-		//bit 2 = Enable Fullcolor for chars >$ff
-		lda #$07
-		sta $d054
-
-
-		//Clear 8 pages (2000 bytes) for Hires and MCM
-		// lda #$00
-		// ldx #$08//Pages to clear
-		// jsr ScreenClear32bitAddr
-		// lda #$08
-		// ldx #$08//Pages to clear
-		// jsr ColorClear32bitAddr
-
-		//Clear 16 pages (4000 bytes) FCM
-		lda #$00
-		ldx #$10//Pages to clear
-		jsr ScreenClear32bitAddr
-		// lda #$08
-		// ldx #$10//Pages to clear
-		// jsr ColorClear32bitAddr
-
-
+		// Turn off CIA interrupts
+		lda #$7f
+		sta $dc0d
+		sta $dd0d
 		
-
-		jsr SetPalette
-		//Set a 16 bit char on screen
-		ldz #$00
+		//Turn off raster interrupts, used by C65 rom
 		lda #$00
-		sta ((ZP.Screen)), z 
-		lda #$01
-		inz 
-		sta ((ZP.Screen)), z 
-
-
-		//change the 16 bit extended atrributes
-		ldz #$00
-		lda #%10000000 //Set bit 7 = Vertical flip
-		sta ((ZP.Color)), z 
-		lda #$00000000
-		inz 
-		sta ((ZP.Color)), z 
-
-		jmp *
-		
+		sta $d01a
+        cli
+        rts
 }
 
-
-SetPalette: {
+ImportPalette: {
 		//Bit 6-7 = Mapped Palette
 		//bit 0-1 = Char palette index
 		lda #%01000001
@@ -128,153 +197,10 @@ SetPalette: {
 		rts
 }
 
+* = $7000
 PaletteData:
-// #4CAF50
+	.import binary "pal.bin"
 
-	//256 reds (only color that is 7 bits not 8)
-	//Missing bit because of nybble swap = bit 4
-	.byte $00,$0f,$00,$00
-	.fill 252, 0
+* = $8000	///Char index = $0200	
+	.import binary "chars.bin"
 
-	//256 greens
-	.byte $00,$00,$0f,$00
-	.fill 252, 0
-
-	//256 blues
-	.byte $00,$00,$00,$0f
-	.fill 252, 0
-
-
-ColorClear32bitAddr: {
-
-	!Outerloop:
-		ldz #$00
-	!:
-		sta ((ZP.Color)), z
-		inz 
-		bne !-
-		dex 
-		beq !+
-		inc ZP.Color + 1
-		bra !Outerloop-
-	!:
-
-		jsr SetupColorVector
-		rts
-}
-
-ScreenClear32bitAddr: {
-		lda #$01
-	!Outerloop:
-		ldz #$00
-	!:
-		sta ((ZP.Screen)), z
-		inz 
-		bne !-
-		dex 
-		beq !+
-		inc ZP.Screen + 1
-		bra !Outerloop-
-	!:
-
-		jsr SetupScreenVector
-		rts
-}
-
-SetupScreenVector: {
-		lda #<SCREEN_RAM
-		sta ZP.Screen + 0
-		lda #>SCREEN_RAM 
-		sta ZP.Screen + 1
-		lda #[SCREEN_RAM >> 16]
-		sta ZP.Screen + 2
-		lda #$00
-		sta ZP.Screen + 3
-		rts 
-}
-
-SetupColorVector: {
-		lda #<COLOR_RAM
-		sta ZP.Color + 0
-		lda #>COLOR_RAM 
-		sta ZP.Color + 1
-		lda #[[COLOR_RAM >> 16] & $ff]
-		sta ZP.Color + 2
-		lda #[[COLOR_RAM >> 24] & $ff]
-		sta ZP.Color + 3
-		rts 
-}
-
-ScreenClear80: {
-		//DEFAULT SCREEN CLEAR
-		lda #$20
-		ldx #$00
-	!:
-		sta $0800, x 
-		sta $0900, x 
-		sta $0a00, x 
-		sta $0b00, x 
-		sta $0c00, x 
-		sta $0d00, x 
-		sta $0e00, x 
-		sta $0f00, x 
-		inx 
-		bne !-
-		rts
-}
-
-
-SetupSystem: {
-		sei 
-		// Set memory layout (c64??)
-		lda #$35
-		sta $01
-
-		enable40Mhz() 
-		enableVIC4Registers()
-
-		//Turn off CIA interrupts
-		lda #$7f
-		sta $dc0d
-		sta $dd0d
-
-		//Turn off raster interrupts, used by C65 rom
-		lda #$00
-		sta $d01a
-
-		
-		//Disable C65 rom write protection
-		//$20000 - $3ffff
- 		lda #$70
-        sta $d640
-        eom
-
-        cli
-        rts
-}
-
-//16 bit char addresses (charnum = addr/64)
-/*
-	$000 = $0000
-	$001 = $0040
-	$002 = $0080
-	...
-	$100 = $4000
-*/
-
-
-* = $4000
-	.fill 8, 0
-	.fill 8, 0
-	.fill 8, 1
-	.fill 8, 1
-	.fill 8, 2
-	.fill 8, 2
-	.fill 8, 3
-	.fill 8, 3
-
-
-* = $8127
-CHARROM:
-	.fill 8, $01
-	.fill 8, $10
--- make.bat
+++ make.bat
@@ -1,7 +1,22 @@
+rem DEPLOY deploys over serial port to Mega6y5 or Nexys boards
 set DEPLOY="C:\C64\Tools\m65_connect\M65Connect Resources\m65.exe" -l COM6 -F -r 
-set KICKASM=java -cp Z:\Projects\Mega65\_build_utils\kickass.jar kickass.KickAssembler65CE02  -vicesymbols -showmem 
+
+rem C1541.exe from vice - used to create a D81 and add files
+set WRITE="C:\C64\Tools\vice\bin\c1541.exe" -attach "./bin/DISK.D81" 8 -write
+set FORMAT="C:\C64\Tools\vice\bin\c1541.exe" -format "turricandemo,0" d81 "./bin/DISK.D81"
+
+set KICKASM=java -cp Z:\Projects\Mega65\_build_utils\kickass519.jar kickass.KickAssembler65CE02  -vicesymbols -showmem 
 %KICKASM%  main.s -odir ./bin 
 
+rem create and write disk
+rem %FORMAT%
+rem %WRITE% "./bin/main.prg" main
+rem %WRITE% "./chars.bin" chars
+rem %WRITE% "./pal.bin" pal
+rem launch XEMU with disk image
+rem "C:\Program Files\xemu\xmega65.exe" -besure -autoload -8 "./bin/DISK.D81"
+
+rem launch XEMU with prg
 "C:\Program Files\xemu\xmega65.exe" -besure -prg "./bin/main.prg"
 
-rem %DEPLOY% "./bin/main.prg"
+%DEPLOY% "./bin/main.prg"
